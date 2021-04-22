7919bf605fafaebbc7eccb89fbb43c7dff1b029b
felexkemboi <felokemboi10@gmial.com>
2021-04-22 18:57:18 +0300

    final source code with query generated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,40 +32,56 @@
                       </div>
                       <div class="space-y-6 sm:space-y-5">
                         <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                          <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Table</label>
-                          <!-- <div class="mt-1 sm:mt-0 sm:col-span-2">
-                            <v-select :options="people"></v-select>
-
-                            <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
-                          </div> -->
+                          <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Database</label>
                           <div class="px-20 mt-4">
-                            <select v-model="selected" class="w-full px-3 py-2 bg-white border rounded outline-none">
-                              <option v-for="option in people" :key="option.id">{{ option.name }}</option>
+                            <select v-model="database" class="w-full px-3 py-2 bg-white border rounded outline-none">
+                              <option v-for="option in databases" :key="option.name" :value="option">{{ option.name }}</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Table</label>
+                          <div class="px-20 mt-4">
+                            <select v-model="table" class="w-full px-3 py-2 bg-white border rounded outline-none">
+                              <option v-for="option in selectedDatabase.tables" :key="option.name" :value="option">{{ option.name }}</option>
                             </select>
                           </div>
                         </div>
 
                         <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                          <label for="last_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Select the columns</label>
-                          <div class="mt-1 sm:mt-0 sm:col-span-2">
-                            <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">columns</label>
+                          <div class="px-20 mt-4">
+                            <select v-model="columns" class="w-full px-3 py-2 bg-white border rounded outline-none">
+                              <option v-for="option in selectedTable.columns" :key="option">{{ option }}</option>
+                            </select>
                           </div>
                         </div>
 
                         <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                          <label for="last_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Conditions</label>
-                          <div class="mt-1 sm:mt-0 sm:col-span-2">
-                            <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md" />
+                          <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Where</label>
+                          <div class="mt-1">
+                            <input type="text" v-model="condition" class="w-full px-3 py-2 bg-white border rounded outline-none" placeholder="e.g 'age>4' or 'age=23' or age<43" />
+                          </div>
+                        </div>
+
+                        <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                          <label for="first_name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Group By</label>
+                          <div class="px-20 mt-4">
+                            <select v-model="groupBy" class="w-full px-3 py-2 bg-white border rounded outline-none">
+                              <option v-for="option in selectedTable.columns" :key="option">{{ option }}</option>
+                            </select>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-
+                  <div v-if="showQuery"  class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                      <div class="px-20 mt-4">{{ query }}</div>
+                    </div>
                   <div class="pt-5">
                     <div class="flex justify-end">
                       <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cancel</button>
-                      <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Pull Records</button>
+                      <button type="submit" @click="pull()" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Pull Records</button>
                     </div>
                   </div>
                 </form>
@@ -85,66 +101,58 @@
 export default {
   name: 'UserHome',
   computed: {
-    name() {
-      return this.$store.state.name
+    selectedDatabase() {
+      return this.database
     },
-    routerPath(){
-      return this.$route.name;
+    selectedTable() {
+      return this.table
     },
-    showSettings(){
-      return (this.routerPath != "non-parent" && this.routerPath != "parent" ) ? true : false
-    },
-    showFamily(){
-      return ( this.routerPath != "parent" && this.routerPath != "non-parent" && this.routerPath != "non-parent-edit" ) ? true : false
+    query(){
+     return  `SELECT ${this.columns} FROM ${this.table.name}.${this.database.name}`
     }
   },
   data(){
     return {
-        routerName: '',
-        people : [
-          { id: 1, name: 'Wade Cooper' },
-          { id: 2, name: 'Arlene Mccoy' },
-          { id: 3, name: 'Devon Webb' },
-          { id: 4, name: 'Tom Cook' },
-          { id: 5, name: 'Tanya Fox' },
-          { id: 6, name: 'Hellen Schmidt' },
-          { id: 7, name: 'Caroline Schultz' },
-          { id: 8, name: 'Mason Heaney' },
-          { id: 9, name: 'Claudie Smitham' },
-          { id: 10, name: 'Emil Schaefer' },
+        showQuery: false,
+        databases : [
+            {
+                name: 'csv',
+                tables: [
+                    {
+                      name: 'categories',
+                      columns: ['id', 'categoryName','description','picture']
+                    },
+                    {
+                      name: 'customers',
+                      columns: ['id', 'customerName','age','picture']
+                    },
+                ]
+            },
+            {
+                name: 'json',
+                tables: [                    
+                    {
+                      name: 'categories',
+                      columns: ['id', 'categoryName','description','picture']
+                    },
+                    {
+                      name: 'customers',
+                      columns: ['id', 'customerName','age','picture']
+                    }
+                ]
+            }
         ],
-        selected : ""
-    }
-  },
-  mounted(){
-    this.setRouterName()
-  },
-  watch: {
-    routerPath(newValue){
-      if(newValue){
-        this.$router.name = this.routerPath
-      }
+        database : "",
+        table: "",
+        columns : "",
+        groupBy : null,
+        where : null
     }
   },
   methods: {
-    family(){
-      this.$router.replace('/user-home/family')
-    },
-    setRouterName(){
-      this.routerName = this.routerPath
-    },
-    settings(){
-      this.$router.replace('/user-home/settings')
-    },
-    profile(){
-      if(this.showSettings == true){
-        this.$router.replace('/user-home/parent-edit')
-      }else{
-        this.$router.replace('/user-home/non-parent-edit')
-      }      
-    },
-    logout(){
-      this.$router.replace('/')
+    pull(){
+      this.showQuery = true
+      setTimeout(() => {  true }, 40000);
     }
   }
 }
